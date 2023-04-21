1c49f107b7b5b99aee398cf11a03f1ffd58e8568
ligh <lightlay80@163.com>
2023-04-20 20:48:35 -0700

    双向链表

--- src/main.c
+++ src/main.c
@@ -6,15 +6,20 @@
 
 struct stu
 {
-int iScore;
-char cName[10];
-struct stu* pNext;
+	int iScore;
+	char cName[10];
+	struct stu* pBefo;
+	struct stu* pNext;
 };
 
 int main()
 {
-	struct stu stu_temp;
+	struct stu* pTemp = NULL;
+	//pTemp用作顺序打印和倒序打印链表时用到的临时指针
 	struct stu* pHead = NULL;
+	//定义pHead用来指向队首结点
+	struct stu* pEnd = NULL;
+
 
 	// struct stu stu1;
 	// stu1.iScore = 89;
@@ -29,31 +34,35 @@ int main()
 	struct stu* stu_new = (struct stu*)malloc(sizeof(struct stu));
 	struct stu* stu_end = NULL;
 
-	//temp指向NEW
-	pHead = &stu_temp;
-	stu_temp.iScore = -1;
-	stu_temp.pNext = stu_new;
+	pHead = stu_new;
 	
 	while (scanf("%d", &stu_new->iScore)&&stu_new->iScore != -1)
 	{
 		scanf("%s", stu_new->cName);
 		stu_end = stu_new;
 		stu_new = (struct stu*)malloc(sizeof(struct stu));
-		stu_end->pNext= stu_new;
+		stu_end->pNext = stu_new;
+		stu_new->pBefo = stu_end;
 	}
+
 	stu_end->pNext = NULL;
 	free(stu_new);
+	pEnd = stu_end;
 
-	while (pHead != NULL)
+	printf("顺序结果打印:\n");
+	pTemp = pHead;
+	while (pTemp != NULL)
 	{
-		if (pHead->iScore == -1)
-		{
-			pHead = pHead->pNext;
-			continue;
-		}
-		
-		printf("%d %s\n", pHead->iScore,  pHead->cName);
-		pHead =  pHead->pNext;
+		printf("%d %s\n", pTemp->iScore,  pTemp->cName);
+		pTemp = pTemp->pNext;
+	}
+
+	printf("倒序结果打印:\n");
+	pTemp = pEnd;
+	while (pTemp != NULL)
+	{
+		printf("%d %s\n", pTemp->iScore,  pTemp->cName);
+		pTemp = pTemp->pBefo;
 	}
 	
     return 0;
